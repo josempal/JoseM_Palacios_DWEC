615abb1fd407314f124ba949af824193a31181e9
jmpl <alum.jmanuelpalaciosl@iesalixar.org>
2020-10-21 19:47:36 +0200

    Mod U3T3

--- U3/U3T3/U3T3.js
+++ U3/U3T3/U3T3.js
@@ -2,4 +2,4 @@
 let opt = prompt("Introduce un número entero: ");
 let num = parseInt(opt);
 console.log(num.toExponential() + " / " + num.toFixed(4) + " / " + num.toString(2) + " / " 
-    + num.toString(8) + " / " + num.toString(16));
+    + num.toString(8) + " / " + '0x' + num.toString(16));
