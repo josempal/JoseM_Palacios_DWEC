6f8f212c0eb9ac6351d35468b255af02930b3865
jmpl <alum.jmanuelpalaciosl@iesalixar.org>
2020-11-25 19:57:10 +0100

    Reorg.

--- U3/U3_Examen/U3_Examen_E1/U3_Ex_E1.js
+++ U3/U3_Examen/U3_Examen_E1/U3_Ex_E1.js
@@ -5,29 +5,41 @@ function Bandeja(tipo_pastel, variedad_pastel, cant_pastel, hora_elab) {
     this.variedad_pastel = variedad_pastel;
     this.cant_pastel = cant_pastel;
     this.hora_elab = hora_elab;
-    this.hora_ult_venta = new Date();
+    this.hora_ult_venta = null;
 
-    /*this.tiempo_venta = function() {
+    this.tiempo_venta = function () {
 
-        let dif = this.hora_ult_venta.getTime() - this.hora_elab.getTime();
+        let dif = this.hora_elab.getTime() -this.hora_ult_venta.getTime();
 
-        const msHora = (60*60) * 1000;
-        const msMin = 60 * 1000;
+        let secs = dif / 1000;
 
-        let horas = dif / msHora;
-        let mins = dif / msMin;
-        let segs = dif / 1000
+        let hours = parseInt(secs / (60 * 60));
+        secs = secs % (60 * 60);
+ 
+        let mins = parseInt(secs / 60);
+     
+        secs = parseInt(secs % 60);
+        
+        return hours + "h:" + mins + "m:" + secs + "s";
+    }
 
-    }*/
-
-    this.restar = function(cant) {
+    this.restar = function (cant) {
 
         if (this.cant_pastel - cant < 0) {
 
             this.cant_pastel = 0;
         } else {
-        this.cant_pastel -= cant 
-       }
+            this.cant_pastel -= cant
+        }
+    }
+
+    this.toString = function () {
+
+        return "Tipo pastel: " + this.tipo_pastel + "| Variedad pastel: " + this.variedad_pastel
+            + "| Cantidad: " + this.cant_pastel + "| Hora elaboración: " + this.hora_elab.getHours() + ":"
+            + this.hora_elab.getMinutes() + ":" + this.hora_elab.getSeconds()
+            + "| Hora última venta: " + this.hora_ult_venta.getHours() + ":"
+            + this.hora_ult_venta.getMinutes() + ":" + this.hora_ult_venta.getSeconds()
     }
 }
 
@@ -35,30 +47,61 @@ function LaPlata() {
 
     this.mostrador = [];
 
-    this.poner_en_mostrador = function(pastel) {
+    this.poner_en_mostrador = function (bandeja) {
 
         let boolpastel = false;
         let booltipo = false;
 
-        for (x of mostrador) {
+        for (x of this.mostrador) {
 
             if (x.tipo_pastel === pastel.tipo_pastel) {
                 boolpastel = true;
             }
-        }
 
-        for (y of mostrador) {
-
-            if (y.variedad_pastel == pastel.tipo_pastel) {
+            if (x.variedad_pastel == pastel.tipo_pastel) {
                 booltipo = true;
             }
-            
         }
 
-        if (boolpastel == false && booltipo == false) {
+        if (boolpastel == false || booltipo == false) {
 
-            this.mostrador.push(pastel)
+            this.mostrador.push(bandeja);
+        }
+    }
+
+    this.vender_pastel = function(tipo, variedad, cant) {
+
+        for (x of this.mostrador) {
+
+            if (x.tipo_pastel === tipo
+                && x.variedad_pastel === variedad
+                && x.cant_pastel > 0) {
+
+                if (x.cant_pastel - cant <= 0) {
+
+                    x.restar(cant);
+                    x.hora_ult_venta = new Date();
+                    console.log(x.tiempo_venta());
+
+                } else {
+
+                    x.restar(cant);
+                    x.hora_ult_venta = new Date();
+                }
+            } else {
+                console.log("No hay bandeja con el pastel seleccionado")
+            }
         }
     }
 }
 
+
+let p1 = new Bandeja("Caracola", "Relleno crema", 15, new Date(2020, 11, 19, 13, 03, 00));
+let laPlata = new LaPlata();
+
+laPlata.poner_en_mostrador(p1);
+
+laPlata.vender_pastel("Caracola", "Relleno crema", 10);
+
+for (x of laPlata.mostrador)
+console.log(x.toString());
--- U3/U3_Examen/U3_Examen_E3/U3_Ex_E3.js
+++ U3/U3_Examen/U3_Examen_E3/U3_Ex_E3.js
@@ -1,29 +1,39 @@
 
-let msg = prompt("Introduce el producto y la zona con el siguiente formato: tipo_producto|modelo|cantidad@cz:")
+let msg = prompt("Introduce el producto y la zona con el siguiente formato: tipo_producto|modelo|cantidad@cz:");
 
-let arrayMsg = msg.split("|");
+if (msg.includes("|") && msg.includes("@") && msg != null) {
 
-let aux = arrayMsg[2].split("@");
-let cant = aux[0];
-let zona = aux[1];
+    let arrayMsg = msg.split("|");
 
-let products = ["televisor", "tablet", "smartphone", "laptop"];
-let model_pattern = new RegExp("[A-Z]{3}[0-9]{3}");
-let zone = ["HU", "CA", "SE"];
+    let aux = arrayMsg[2].split("@");
+    let cant = aux[0];
+    let zona = aux[1];
 
-if (checkProducts(arrayMsg[0])
+    let products = ["televisor", "tablet", "smartphone", "laptop"];
+    let model_pattern = new RegExp("[A-Z]{3}[0-9]{3}");
+    let zone = ["HU", "CA", "SE"];
+
+    if (checkProducts(arrayMsg[0])
         && model_pattern.test(arrayMsg[1])
         && isNaN(cant) === false
         && checkZone(zona)) {
 
-        document.getElementById("prod").innerHTML = "Tipo producto: " + arrayMsg[0]; 
+        document.getElementById("prod").innerHTML = "Tipo producto: " + arrayMsg[0];
         document.getElementById("mod").innerHTML = "Modelo: " + arrayMsg[1];
         document.getElementById("cant").innerHTML = "Cantidad: " + cant;
-        document.getElementById("zon").innerHTML = "Zona: " + zone;   
+        document.getElementById("zon").innerHTML = "Zona: " + zona;
+    } else {
+        alert("Los datos introducidos son incorrectos");
+    }
+} else {
+    alert("El formato es incorrecto");
 }
+/*  console.log(checkProducts(arrayMsg[0]));
+ console.log(model_pattern.test(arrayMsg[1]));
+ console.log(isNaN(cant));
+ console.log(checkZone(zona)); */
 
-
-function checkProducts (prod) {
+function checkProducts(prod) {
 
     let res = false;
 
